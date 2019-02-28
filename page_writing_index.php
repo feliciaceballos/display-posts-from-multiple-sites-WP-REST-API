598a8a81e143affaaf0f7171be05f8cc4f37cfa7
Felicia Ceballos-Marroquin <felicia@feliciaceballos.com>
2019-02-28 15:03:26 -0800

    Create page_writing_index.php

---
+++ page_writing_index.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Template Name: Writing Index
+*/
+
+
+//* Adds a CSS class to the body element
+add_filter( 'body_class', 'bbs_index_body_class' );
+function bbs_index_body_class( $classes ) {
+
+	$classes[] = 'bbs-index';
+	return $classes;
+
+}
+
+add_action( 'genesis_entry_content', 'fc_display_posts_from_other_blogs' );
+
+genesis();
